7d6e4181e3960ed0f5dae43908e86e45f291dbf3
taiwo2 <adetonat@ymail.com>
2021-02-01 16:21:11 +0100

    update ruby version

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,7 +3,7 @@ class Author < ApplicationRecord
 
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = '/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i'
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 244 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
