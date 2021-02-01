5136b2eafd0a968a6104db782025217672ce94e6
taiwo2 <adetonat@ymail.com>
2021-02-01 13:42:45 +0100

    Add post database

--- db/migrate/20210201122303_create_posts.rb
+++ db/migrate/20210201122303_create_posts.rb
@@ -7,5 +7,6 @@ class CreatePosts < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
+    add_index :posts, [:user_id, :created_at]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_121719) do
+ActiveRecord::Schema.define(version: 2021_02_01_122303) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -20,4 +20,15 @@ ActiveRecord::Schema.define(version: 2021_02_01_121719) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "authors_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index "\"user_id\", \"created_at\"", name: "index_posts_on_user_id_and_created_at"
+    t.index ["authors_id"], name: "index_posts_on_authors_id"
+  end
+
+  add_foreign_key "posts", "authors", column: "authors_id"
 end
