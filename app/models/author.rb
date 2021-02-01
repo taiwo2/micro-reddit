af394f0724ed24af06a9a9cab20088cc4a4340e7
taiwo2 <adetonat@ymail.com>
2021-02-01 14:02:29 +0100

    Add Update Author class

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ApplicationRecord
+  has_many :posts, dependent: :destroy
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
