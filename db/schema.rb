76a75a6fde432d4fa2da3baaa1f1ea8e7194d548
taiwo2 <adetonat@ymail.com>
2021-02-01 13:18:32 +0100

    Add unique email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_120000) do
+ActiveRecord::Schema.define(version: 2021_02_01_121719) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
